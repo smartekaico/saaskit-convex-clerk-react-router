b130c1ed69b72c59a0caa87cab97aee0398ffe76
Amer <amer1616@gmail.com>
2026-04-22 12:16:29 +0300

    Fix: import index.css in main.tsx for Tailwind styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode, startTransition } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router';
+import './index.css';
 
 startTransition(() => {
   createRoot(document.getElementById('root')!).render(
